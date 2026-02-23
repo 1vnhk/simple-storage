cda7f7390ec354e5c7f47d81434bd24596b548a3
Ivan Hrekov <iandrc9@gmail.com>
2026-02-23 18:41:23 +0100

    feat: add access control
    - Only owner of the contract can change their favorite number
    - Only owner of the contract can add people
    
    The two points above are achieved through the use of `modifier`
    
    - Introduce `error` instead of string for gas optimization
    - Move `Person` struct out of the contract logic
    - Make `owner` field immutable for gas optimization
    - Remove `retrieve` and subsitute it with a default getter

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,25 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+error NotOwner();
+
+struct Person {
+    uint256 favoriteNumber;
+    string name;
+}
+
 contract SimpleStorage {
     uint256 public myFavoriteNumber;
-
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
 
     Person[] public listOfPeople;
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function store(uint256 _favoriteNumber) public onlyOwner {
         myFavoriteNumber = _favoriteNumber;
     }
 
-    function addPerson(string calldata _name, uint256 _favoriteNumber) public {
+    function addPerson(
+        string calldata _name,
+        uint256 _favoriteNumber
+    ) public onlyOwner {
         listOfPeople.push(
             Person({name: _name, favoriteNumber: _favoriteNumber})
         );
         nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
     }
 }
